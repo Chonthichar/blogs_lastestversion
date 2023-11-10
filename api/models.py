8d6fbf0c05cdeaa3940983d083c8a0a2a94be2dc
Chonthichar <84187224+Chonthichar@users.noreply.github.com>
2023-11-10 22:10:21 +0100

    Add files via upload

---
+++ api/models.py
@@ -0,0 +1,14 @@
+# from django.db import models
+# from tastypie.resources import ModelResource
+# # from blog_and_vlog.blog.models import models
+# from blog.models import Fields
+#
+#
+#
+# # Create your models here.
+# # As models as resources
+#
+# class BlogResource(ModelResource):
+#     class Meta:
+#         queryset = Fields.objects.all()
+#         resource_name = 'blog'
